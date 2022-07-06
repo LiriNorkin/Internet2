fef1a5e5314b9ba7fed67cd896b50cafbd78c9ea
Tamir Sadovsky <tamirsadovsky@gmail.com>
2022-07-06 11:08:25 +0300

    bm-25 score added

--- vsm_ir.py
+++ vsm_ir.py
@@ -1,12 +1,15 @@
 import os
 import sys
 import xml.etree.ElementTree as ET
+from collections import Counter
+
 import nltk
 import math
 import json
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
+import numpy as np
 
 corpus = {}
 dict_tfidf = {} # word : tf-idf score
@@ -14,6 +17,8 @@ dict_bm25 = {} # word : bm-25 score
 dict_query = {}
 doc_vec_len = {} # record id : len of document vector
 words_num_in_file = {} # file_name : num of words in file after tokenization
+tf_for_bm25 = {} # save tf for bm-25 score
+avgdl = 0 # average document length in the corpus
 
 ### PART 1: Inverted Index and scores #################################################################
 
@@ -37,6 +42,8 @@ def update_dictionary(text, record_num, file):
 
 
 def extract_text(xml_root, token, stop_words, ps, file):
+    global tf_for_bm25
+    global avgdl
     for record in xml_root.findall("./RECORD"):
         text = ""
         text_without_stopwords = []
@@ -60,6 +67,10 @@ def extract_text(xml_root, token, stop_words, ps, file):
 
         update_dictionary(text_without_stopwords, record_num, file)
         words_num_in_file[record_num] = len(text)
+        print(words_num_in_file)
+        tf_for_bm25 = dict_tfidf.copy()
+        #print(tf_for_bm25)
+        #avgdl = np.array(list(words_num_in_file.values())).mean()
 
 
 def extract_words(file):
@@ -94,7 +105,8 @@ def calculate_bm25(docs_num):
     pass
 
 def save_inverted_index_file():
-    corpus["bm25_dict"] = dict_bm25
+    #print(tf_for_bm25)
+    corpus["bm25_dict"] = words_num_in_file
     corpus["tfidf_dict"] = dict_tfidf
     corpus["document_vector_len"] = doc_vec_len
 
@@ -107,9 +119,13 @@ def create_index(xmls_dir):
     input_dir = sys.argv[2]
     for file_name in os.listdir(xmls_dir):
         if file_name.endswith(".xml"):
-            file=input_dir+"/"+file_name
+            file = input_dir+"/"+file_name
+            #print(file)
             extract_words(file)
     docs_num = len(doc_vec_len)
+    #print(doc_vec_len)
+    #print(docs_num)
+    #print(dict_tfidf)
 
     calculate_tfidf(docs_num)
 
@@ -118,13 +134,90 @@ def create_index(xmls_dir):
     for file in doc_vec_len:
         doc_vec_len[file] = math.sqrt(doc_vec_len[file])
 
+    #print((doc_vec_len))
+
     save_inverted_index_file()
 
 #######################################################################################################
 ### PART 2: Retrieval information for given query #####################################################
 
+def calc_number_of_docs_containing_term(inverted_index, the_query):
+    dict_counter_occurrence = {}
+    counter = 0
+    for word in the_query:
+        if inverted_index.get(word) != None:
+            for i in inverted_index[word]:
+                counter += 1
+                #print(inverted_index[word][i]["count"])
+            dict_counter_occurrence[word] = counter
+            counter = 0
+    return dict_counter_occurrence
 
-def calc_query_bm25(the_query, inverted_index, docs_num):
+def calculate_idf(word, docs_num, n_qi):
+    return np.log((docs_num - n_qi + 0.5) / (n_qi + 0.5) + 1)
+
+def calculate_idf_dict(query, docs_num, dict_counter_occurrence):
+    dict = {}
+    for word in query:
+        dict[word] = np.log((docs_num - dict_counter_occurrence[word] + 0.5) / (dict_counter_occurrence[word] + 0.5) + 1)
+    return dict
+
+def calc_numerator_bm25(idf, freq_word_doc, k):
+    return idf * freq_word_doc * (k+1)
+
+def calc_denominator_bm25(freq_word_doc, k, b, doc_length, avgdl):
+    return freq_word_doc + k * (1-b + b * (doc_length / avgdl))
+
+def calculate_avgdl(dict_doc_lengths):
+    return np.array(list(dict_doc_lengths.values())).mean()
+
+def calc_bm25(freq_word_doc, k, b, doc_length, avgdl, idf):
+    numerator = calc_numerator_bm25(idf, freq_word_doc, k)
+    denominator = calc_denominator_bm25(freq_word_doc, k, b, doc_length, avgdl)
+    return numerator / denominator
+
+
+def calc_query_bm25(the_query, inverted_index, dict_doc_lengths, docs_num):
+    sorted = {}
+    dict_for_bm25 = {}
+    results_for_bm25 = []
+    avgdl = calculate_avgdl(dict_doc_lengths)
+    dict_counter_occurrence = calc_number_of_docs_containing_term(inverted_index, the_query)
+    dict_idf = calculate_idf_dict(the_query, docs_num, dict_counter_occurrence)
+
+    # k in range [1.2,2] - saw in lecture, usually k = 1.2
+    k = 1.2
+    # b in range [0,1] - saw in lecture, usually b = 0.75
+    b = 0.75
+    query_length = len(the_query)
+    sum = 0
+    freq_word_doc = 0
+
+    for i in range(1, 1240):
+        #print(i)
+        for word in the_query:
+            #print(word)
+            idf = dict_idf[word]
+            #print(inverted_index.get(word))
+
+            if inverted_index.get(word) != None:
+                for j in inverted_index[word]:
+                    #print(j)
+                    if str(i) == j:
+                        #print(i)
+                        freq_word_doc = inverted_index[word][j]["count"]
+                        bm25 = calc_bm25(freq_word_doc, k, b, dict_doc_lengths[j], avgdl, idf)
+                        if j in dict_for_bm25:
+                            dict_for_bm25[j] += bm25
+                        else:
+                            dict_for_bm25[j] = bm25
+    #exit()
+    #print(dict_for_bm25)
+    #print(max(dict_for_bm25, key=dict_for_bm25.get))
+    k = Counter(dict_for_bm25)
+    high = k.most_common(10)
+    for i in high:
+        print(i[0], " :", i[1], " ")
     pass
 
 
@@ -165,7 +258,9 @@ def calc_results(inverted_index, doc_reference, rank_type):
 
                 documents_vectors[doc] += (inverted_index[token][doc][rank_type] * w)
 
+    #print(documents_vectors)
     for doc in documents_vectors:
+        #print(doc)
         doc_query_product = documents_vectors[doc]
         doc_len = doc_reference[doc]
         cosSim = doc_query_product / (doc_len * query_len)
@@ -197,8 +292,10 @@ def query():
 
     corpus = json.load(json_file) # Insert the json file to the global dictionary.
 
-    inverted_index = corpus[dict_type]
+    inverted_index = corpus["tfidf_dict"]
+    #print(inverted_index)
     doc_reference = corpus["document_vector_len"]
+    #print(doc_reference)
     docs_num = len(doc_reference)
     json_file.close()
 
@@ -226,8 +323,10 @@ def query():
 
     # calculate scores for query
     if ranking == "bm25":
-        calc_query_bm25(the_query, inverted_index, docs_num)
+        dict_doc_lengths = corpus["bm25_dict"]
+        calc_query_bm25(the_query, inverted_index, dict_doc_lengths, docs_num)
     elif ranking == "tfidf":
+        #print(inverted_index)
         calc_query_tfidf(the_query, inverted_index, docs_num)
 
     ans_docs = calc_results(inverted_index, doc_reference, ranking)
